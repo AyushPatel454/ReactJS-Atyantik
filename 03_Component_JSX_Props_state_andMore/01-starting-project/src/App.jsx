69aaec21a2447491a0255e165f40debad33d1827
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-05 15:00:42 +0530

    Component Composition: The special 'children' Prop

--- 03_Component_JSX_Props_state_andMore/01-starting-project/src/App.jsx
+++ 03_Component_JSX_Props_state_andMore/01-starting-project/src/App.jsx
@@ -1,7 +1,7 @@
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcepts from "./components/CoreConcepts.jsx";
-
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
   return (
@@ -13,7 +13,6 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            
             <CoreConcepts
              image={CORE_CONCEPTS[0].image}
              title={CORE_CONCEPTS[0].title}
@@ -26,6 +25,17 @@ function App() {
             <CoreConcepts {...CORE_CONCEPTS[3]} />
           </ul>
 
+        </section>
+
+        <section id="examples">
+          <h2>Examples</h2>
+
+          <menu>
+            <TabButton>Component</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
