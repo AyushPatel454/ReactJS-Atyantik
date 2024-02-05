504044e198ac19287ac58d0c6a32ea18e3e264f7
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-05 16:56:08 +0530

    Condition rendering: 1

--- 03_Component_JSX_Props_state_andMore/01-starting-project/src/App.jsx
+++ 03_Component_JSX_Props_state_andMore/01-starting-project/src/App.jsx
@@ -7,29 +7,30 @@ import { EXAMPLES } from "./data.js";
 
 function App() {
   // const [variable, function] = useState(initialValue);
-  const [selectedTopic, setSelectedTopic] = useState("components");
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleClick(selectedButton) {
     // selectedButton => component, jsx, props, state
 
     // selectedTopic = selectedButton; // ---> Not the correct way to update the state.
-    
-      setSelectedTopic(selectedButton); // update the state & re-render the component.      
+
+    setSelectedTopic(selectedButton); // update the state & re-render the component.
   }
 
   return (
     <div>
       {/* load component HTML */}
       <Header />
-      
+
       <main>
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
             <CoreConcepts
-             image={CORE_CONCEPTS[0].image}
-             title={CORE_CONCEPTS[0].title}
-             description={CORE_CONCEPTS[0].description} />
+              image={CORE_CONCEPTS[0].image}
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+            />
 
             <CoreConcepts {...CORE_CONCEPTS[1]} />
 
@@ -37,28 +38,32 @@ function App() {
 
             <CoreConcepts {...CORE_CONCEPTS[3]} />
           </ul>
-
         </section>
 
         <section id="examples">
           <h2>Examples</h2>
 
           <menu>
-            <TabButton onSelect={() => handleClick("components")}>Component</TabButton>
+            <TabButton onSelect={() => handleClick("components")}>
+              Component
+            </TabButton>
             <TabButton onSelect={() => handleClick("jsx")}>JSX</TabButton>
             <TabButton onSelect={() => handleClick("props")}>Props</TabButton>
             <TabButton onSelect={() => handleClick("state")}>State</TabButton>
           </menu>
 
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+          {/* Conditional rendering */}
+          {!selectedTopic ? (
+            <p>Select a topic to see the example</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
         </section>
       </main>
     </div>
