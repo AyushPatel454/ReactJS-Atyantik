c1fc3b5aaf154d6a66223e65df68de844ff34689
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-05 11:58:14 +0530

    Adding dynamic img src attribute value
    By using importing image file.

--- 03_Component_JSX_Props_state_andMore/01-starting-project/src/App.jsx
+++ 03_Component_JSX_Props_state_andMore/01-starting-project/src/App.jsx
@@ -1,3 +1,5 @@
+import reactImg from "./assets/react-core-concepts.png";
+
 const reactDescription = ["Fundamental", "Crucial", "Core"];
 
 function getRandomInt(max) {
@@ -11,7 +13,7 @@ function Header() {
 
   return (
     <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImg} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
           {description} React concepts you will need for almost any app you are
