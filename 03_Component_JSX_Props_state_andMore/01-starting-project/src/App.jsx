9d08b1c64553415e42bdb5ddd39e7fcd31f83b02
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-05 14:26:09 +0530

    Add style to specific component
    - In this case style is also apply outside the component. (We will disscuss before how to make scope of CSS only with in Component)

--- 03_Component_JSX_Props_state_andMore/01-starting-project/src/App.jsx
+++ 03_Component_JSX_Props_state_andMore/01-starting-project/src/App.jsx
@@ -1,5 +1,5 @@
 import { CORE_CONCEPTS } from "./data.js";
-import Header from "./components/Header.jsx";
+import Header from "./components/Header/Header.jsx";
 import CoreConcepts from "./components/CoreConcepts.jsx";
 
 
--- 03_Component_JSX_Props_state_andMore/01-starting-project/src/components/Header/Header.jsx
+++ 03_Component_JSX_Props_state_andMore/01-starting-project/src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
-import reactImg from "../assets/react-core-concepts.png";
+import reactImg from "../../assets/react-core-concepts.png";
+import "./Header.css";
 
 const reactDescription = ["Fundamental", "Crucial", "Core"];
 
