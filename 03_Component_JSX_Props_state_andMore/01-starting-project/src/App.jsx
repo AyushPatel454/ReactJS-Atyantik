27dd2b1ff2509cc0e393cafb0fc4523de2c6a987
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-05 17:23:22 +0530

    CSS Styling & Dynamic Styling
    Je button select karyu hoy eno background color blue kari devo je thi fill ave k aa button select karyu.

--- 03_Component_JSX_Props_state_andMore/01-starting-project/src/App.jsx
+++ 03_Component_JSX_Props_state_andMore/01-starting-project/src/App.jsx
@@ -58,12 +58,10 @@ function App() {
           <h2>Examples</h2>
 
           <menu>
-            <TabButton onSelect={() => handleClick("components")}>
-              Component
-            </TabButton>
-            <TabButton onSelect={() => handleClick("jsx")}>JSX</TabButton>
-            <TabButton onSelect={() => handleClick("props")}>Props</TabButton>
-            <TabButton onSelect={() => handleClick("state")}>State</TabButton>
+            <TabButton isSelected={selectedTopic === "components"} onSelect={() => handleClick("components")}>Component</TabButton>
+            <TabButton isSelected={selectedTopic === "jsx"} onSelect={() => handleClick("jsx")}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === "props"} onSelect={() => handleClick("props")}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === "state"} onSelect={() => handleClick("state")}>State</TabButton>
           </menu>
 
           {/* Conditional rendering */}
--- 03_Component_JSX_Props_state_andMore/01-starting-project/src/components/TabButton.jsx
+++ 03_Component_JSX_Props_state_andMore/01-starting-project/src/components/TabButton.jsx
@@ -1,8 +1,8 @@
-export default function TabButton({children, onSelect}) {
+export default function TabButton({children, onSelect, isSelected}) {
     
     return (
         <li>
-            <button onClick={onSelect}>{children}</button>
+            <button className={isSelected ? "active" : undefined} onClick={onSelect}>{children}</button>
         </li>
     );
 }
