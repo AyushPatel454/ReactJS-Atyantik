b96d977a956d6e7ccf20e7687f2b8903143b5426
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-05 19:11:55 +0530

    Remove un-necessary <div> using modern approach
    function App() {
      return (
         <>
            <h1> ... ... ... </h1>
            <Component />
         </>
      );
    }

--- 03_Component_JSX_Props_state_andMore/01-starting-project/src/App.jsx
+++ 03_Component_JSX_Props_state_andMore/01-starting-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, Fragment } from "react";
+import { useState } from "react";
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcepts from "./components/CoreConcepts.jsx";
@@ -32,7 +32,7 @@ function App() {
   }
 
   return (
-    <Fragment>
+    <>
       {/* load component HTML */}
       <Header />
 
@@ -84,7 +84,7 @@ function App() {
           {tabContent}
         </section>
       </main>
-    </Fragment>
+    </>
   );
 }
 
