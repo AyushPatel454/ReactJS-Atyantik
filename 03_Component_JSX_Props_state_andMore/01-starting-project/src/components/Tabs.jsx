81871c0c958b3a6cf89a0c8e23df2024a2cb6701
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-08 12:24:50 +0530

    Setting default Prop value.

--- 03_Component_JSX_Props_state_andMore/01-starting-project/src/components/Tabs.jsx
+++ 03_Component_JSX_Props_state_andMore/01-starting-project/src/components/Tabs.jsx
@@ -1,5 +1,5 @@
 // This will use for showing the Tabs and the content of the tabs. (example:- navbar and the content of the navbar)
-export default function Tabs({children, buttons, buttonsContainer}) {
+export default function Tabs({children, buttons, buttonsContainer = "menu"}) {
     const ButtonContainer = buttonsContainer; // <menu>...</menu> // convert into custom tag.
     return (
         <>
