a3ca1bb9eb0cdc848a8c13a5c1d72676bd652f8e
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-08 14:49:37 +0530

    Creating Player component
    For display Player 1 & Player 2 before the Game board

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,26 +1,18 @@
+import Player from "./components/Player";
 
 function App() {
-  
-
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <li>
-            <span className="player-name">Player 1</span>
-            <span className="player-symbol">X</span>
-          </li>
-          <li>
-            <span className="player-name">Player 2</span>
-            <span className="player-symbol">0</span>
-          </li>
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="0" />
         </ol>
-
         GAME BOARD
       </div>
       LOG
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
