da605702fa498ac77bfa93aa9064692659b36d60
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-08 15:22:45 +0530

    Working with state.
        When user click on edit button that time show input text field other wise show player name.
        In this we use useState(boolean)
        We store the state of isEditing or not. If user clik on button then isEditing = true & input filed is display. &
        If user again click on edit button that time isEditing = false & input filed is hide & Player name is display.
        Toggle button. - if isEditing = true then ---> Save, if isEditing = false then ---> Edit

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,6 +1,7 @@
 import Player from "./components/Player";
 
 function App() {
+
   return (
     <main>
       <div id="game-container">
--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(!isEditing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  if (isEditing) {
+    playerName = <input type="text" required value={name} />;
+  }
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
+
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing? "Save" : "Edit"}</button>
     </li>
   );
 }
