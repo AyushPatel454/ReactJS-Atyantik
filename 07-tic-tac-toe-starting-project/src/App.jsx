ce979a34861a7c874ef6e73386ee0cf7e5b9bfd8
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-08 14:40:52 +0530

    Display Player 1 & 2 name (Hard coded)

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -3,7 +3,23 @@ function App() {
   
 
   return (
-    <p>Coming soon...</p>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">0</span>
+          </li>
+        </ol>
+
+        GAME BOARD
+      </div>
+      LOG
+    </main>
   )
 }
 
