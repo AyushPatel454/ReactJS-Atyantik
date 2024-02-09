e2181d80a6898c3d1f470029434037e62f6a57f6
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-09 19:07:18 +0530

    Dynamic & Conditional styling using Vanilla CSS - with CSS files & CSS classes

--- 09-styling_ract_component/src/components/AuthInputs.jsx
+++ 09-styling_ract_component/src/components/AuthInputs.jsx
@@ -24,21 +24,21 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <div className="controls">
         <p>
-          <label>Email</label>
+          <label className={`label ${emailNotValid ? 'invalid': ''}`}>Email</label>
           <input
             type="email"
-            style={{
-              backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db', // Conditional styling
-            }}
-            // className={emailNotValid ? 'invalid' : undefined}
+            // style={{
+            //   backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db', // Conditional styling
+            // }}
+            className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
-          <label>Password</label>
+          <label className={`label ${passwordNotValid ? 'invalid': ''}`}>Password</label>
           <input
             type="password"
-            // className={passwordNotValid ? 'invalid' : undefined}
+            className={passwordNotValid ? 'invalid' : undefined}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
