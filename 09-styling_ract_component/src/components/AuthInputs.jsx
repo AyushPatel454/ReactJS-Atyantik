7ce9f3ab2c7150fa5ad02e96ea3fa01f4ecda845
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-12 10:58:56 +0530

    Creating flexible component with styled component.
    all props to the styled component is automaticlly forwared to elemen.
    
    in styled-components, all unknown props are automatically forwarded to the DOM element. This means that if you pass a prop that isn't explicitly handled by the styled component, it will be passed on to the rendered DOM element.
    
    For example, in your code, the type, className, and onChange props are all passed to the Input styled component. These props are not handled by the styled component itself, so they are forwarded to the input element in the DOM.
    
    However, it's important to note that this only applies to DOM elements. If you're styling a React component, the props are not automatically forwarded. You would need to explicitly pass the props to the underlying DOM element in the component.
    
    Also, be aware that React has a list of reserved props (like key, ref, etc.) that are not forwarded, as they have special meanings in React.

--- 09-styling_ract_component/src/components/AuthInputs.jsx
+++ 09-styling_ract_component/src/components/AuthInputs.jsx
@@ -6,7 +6,28 @@ const ControlContainer = styled.div`
   flex-direction: column;
   gap: 0.5rem;
   margin-bottom: 1.5rem;
-`
+`;
+
+const Label = styled.label`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 0.75rem;
+  font-weight: 700;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  color: #6b7280;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 0.75rem 1rem;
+  line-height: 1.5;
+  background-color: #d1d5db;
+  color: #374151;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -32,8 +53,8 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlContainer>
         <p>
-          <label className={`label ${emailNotValid ? 'invalid': ''}`}>Email</label>
-          <input
+          <Label className={`label ${emailNotValid ? 'invalid': ''}`}>Email</Label>
+          <Input
             type="email"
             // style={{
             //   backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db', // Conditional styling
@@ -43,8 +64,8 @@ export default function AuthInputs() {
           />
         </p>
         <p>
-          <label className={`label ${passwordNotValid ? 'invalid': ''}`}>Password</label>
-          <input
+          <Label className={`label ${passwordNotValid ? 'invalid': ''}`}>Password</Label>
+          <Input
             type="password"
             className={passwordNotValid ? 'invalid' : undefined}
             onChange={(event) =>
