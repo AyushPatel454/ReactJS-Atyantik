301bd5b3c6f7191624c0b6559f63869b0e690d8c
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-12 10:41:35 +0530

    Styled Components (3rd party package)
    - npm install styled-components
    - import { styled } from 'styled-components';
    
    styled.property_name ---> There is many property like: div,h1,h2, ... etc.
    
    (Tag Template)
    const ControlContainer = styled.div
      display: flex;
      flex-direction: column;
      gap: 0.5rem;
      margin-bottom: 1.5rem;
    
    <ControlContainer>
        ... ... ...
        ... ... ...
        ... ... ...
    </ControlContainer>

--- 09-styling_ract_component/src/components/AuthInputs.jsx
+++ 09-styling_ract_component/src/components/AuthInputs.jsx
@@ -1,4 +1,12 @@
 import { useState } from 'react';
+import { styled } from 'styled-components';
+
+const ControlContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -22,7 +30,7 @@ export default function AuthInputs() {
 
   return (
     <div id="auth-inputs">
-      <div className="controls">
+      <ControlContainer>
         <p>
           <label className={`label ${emailNotValid ? 'invalid': ''}`}>Email</label>
           <input
@@ -44,7 +52,7 @@ export default function AuthInputs() {
             }
           />
         </p>
-      </div>
+      </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
