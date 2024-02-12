185395c9d9a8379892287ddb1bbedcda46b26c4b
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-12 15:01:57 +0530

    Styling Form (AuthInputs.jsx) file.

--- 09-styling_ract_component/src/components/AuthInputs.jsx
+++ 09-styling_ract_component/src/components/AuthInputs.jsx
@@ -1,14 +1,6 @@
 import { useState } from "react";
-import { styled } from "styled-components";
 import Button from "./Button.jsx";
 import Input from "./Input.jsx";
-
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -31,8 +23,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div id="auth-inputs" className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+      <div className="flex flex-col gap-2 mb-6 ">
           <Input
             label="Email"
             invalid={emailNotValid}
@@ -47,9 +39,9 @@ export default function AuthInputs() {
               handleInputChange("password", event.target.value)
             }
           />
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button onClick={handleLogin}>
