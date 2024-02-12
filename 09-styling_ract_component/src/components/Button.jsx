5904cf4267126922ca56bf5e06b0c9319141c604
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-12 14:27:33 +0530

    Styling Button using tailwind CSS

--- 09-styling_ract_component/src/components/Button.jsx
+++ 09-styling_ract_component/src/components/Button.jsx
@@ -1,18 +1,5 @@
-import { styled } from 'styled-components';
-
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
-
-  &:hover {
-    background-color: #f0920e;
-  }
-`;
-
-export default Button;
+export default function Button({children, ...props}) {
+  return (
+    <button {...props} className="px-4 py-2 font-semibold uppercase rounded text-stone-900 bg-amber-400 hover:bg-amber-500">{children}</button>
+  );
+}
