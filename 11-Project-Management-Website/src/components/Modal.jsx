759f12a75b58987cf6b1644751dbf1503ffc3b76
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-13 16:39:01 +0530

    Validating user input & Showing and Error Modal via useImperativeHandle.
    - Modal is in with in modal-root element we use createPortal the code.
    - use children props for display other Dialog also.

---
+++ 11-Project-Management-Website/src/components/Modal.jsx
@@ -0,0 +1,25 @@
+import { createPortal } from "react-dom";
+import { forwardRef, useImperativeHandle, useRef } from "react";
+
+const Modal = forwardRef(function Modal({children, buttonCaption}, ref) {
+    const dialog = useRef();
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                dialog.current.showModal();
+            }
+        }
+    });
+
+    return createPortal(
+        <dialog ref={dialog}>
+            {children}
+            <form method="dialog">
+                <button>{buttonCaption}</button>
+            </form>
+        </dialog>,
+        document.getElementById("modal-root")
+    );
+});
+
+export default Modal;
--- 11-Project-Management-Website/src/components/NewProject.jsx
+++ 11-Project-Management-Website/src/components/NewProject.jsx
@@ -1,36 +1,67 @@
 import { useRef } from "react";
 import Input from "./Input.jsx";
+import Modal from "./Modal.jsx";
 
-export default function NewProject({onAdd}) {
-    const title = useRef();
-    const description = useRef();
-    const dueDate = useRef();
+export default function NewProject({ onAdd }) {
+  const modal = useRef();
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
 
-    function handleSave() {
-        const enteredTitle = title.current.value;
-        const enteredDescription = description.current.value;
-        const enteredDueDate = dueDate.current.value;
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
 
-        // Validation ...
-
-        onAdd({
-            title: enteredTitle,
-            description: enteredDescription,
-            dueDate: enteredDueDate,
-        });
+    // Validation ...
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredDueDate.trim().length === 0
+    ) {
+      // Show error modal
+      modal.current.open();
+      return;
     }
 
-    return (
-        <div className="w-[35rem] mt-16">
-            <menu className="flex items-center justify-end gap-4 my-4">
-                <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
-                <li><button onClick={handleSave} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
-            </menu>
-            <div>
-                <Input type="text" ref={title} label="Project Name" />
-                <Input ref={description} label="Project Description" textArea />
-                <Input type="date" ref={dueDate} label="Due Date" />
-            </div>
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
+  }
+
+  return (
+    <>
+      <Modal ref={modal} buttonCaption="Okay">
+        <h2>Invalid Inputs</h2>
+        <p>Oops ... looks like you forgot to enter value.</p>
+        <p>
+          Please make sure you provide a valid value for every input field.
+        </p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={handleSave}
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={title} label="Project Name" />
+          <Input ref={description} label="Project Description" textArea />
+          <Input type="date" ref={dueDate} label="Due Date" />
         </div>
-    );
+      </div>
+    </>
+  );
 }
