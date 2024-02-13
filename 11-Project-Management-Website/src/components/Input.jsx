97396c443eaec01a3b01f7745e3c194acae9cd9f
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-13 14:53:00 +0530

    Styling Button & Inputs with tailwind CSS
    (Style previous commit component)

--- 11-Project-Management-Website/src/components/Input.jsx
+++ 11-Project-Management-Website/src/components/Input.jsx
@@ -1,8 +1,9 @@
 export default function Input({ label, textArea, ...props }) {
+    const classes = "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
   return (
-    <p>
-      <label>{label}</label>
-      {textArea ? <textarea {...props}></textarea> : <input {...props} />}
+    <p className="flex flex-col gap-1 my-4">
+      <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
+      {textArea ? <textarea {...props} className={classes} ></textarea> : <input {...props} className={classes} />}
     </p>
   );
 }
--- 11-Project-Management-Website/src/components/NewProject.jsx
+++ 11-Project-Management-Website/src/components/NewProject.jsx
@@ -2,10 +2,10 @@ import Input from "./Input";
 
 export default function NewProject() {
     return (
-        <div>
-            <menu>
-                <li><button>Cancel</button></li>
-                <li><button>Save</button></li>
+        <div className="w-[35rem] mt-16">
+            <menu className="flex items-center justify-end gap-4 my-4">
+                <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
+                <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
             </menu>
             <div>
                 <Input label="Project Name" />
