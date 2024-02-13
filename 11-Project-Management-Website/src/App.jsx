4dca260db175827261392e9b5cb96b9e407d5975
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-13 14:24:32 +0530

    Add Projects Sidebar component with tailwind css styling.

--- 11-Project-Management-Website/src/App.jsx
+++ 11-Project-Management-Website/src/App.jsx
@@ -1,7 +1,11 @@
+import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
+
 function App() {
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <main className="h-screen my-8 flex gap-8">
+        <ProjectsSidebar />
+      </main>
     </>
   );
 }
