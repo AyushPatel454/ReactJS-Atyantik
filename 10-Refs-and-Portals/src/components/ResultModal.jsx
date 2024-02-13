ada7699c42f140ae1c306b07abfa054d29f8146a
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-13 10:57:11 +0530

    Implemented useImperativeHandle in ResultModal component to expose the 'open' method to parent components.
    useImperativeHandle is a built-in React hook that allows you to customize the instance value that is exposed to parent components when using ref.
    
    - It is used when you want to expose specific properties or methods of a child component to a parent component. It's often used in conjunction with forwardRef. (used with forwardRef.)
    
    Here's how to use it:-
    - Wrap your component with forwardRef.
    - Use useImperativeHandle inside your component. The first argument is the ref provided by forwardRef, and the second argument is a function that returns the value for the ref.
    
    In your code, useImperativeHandle is used to expose the open method to parent components. This method, when called, opens the dialog by calling showModal on the dialog ref. This allows parent components to control the dialog directly.

--- 10-Refs-and-Portals/src/components/ResultModal.jsx
+++ 10-Refs-and-Portals/src/components/ResultModal.jsx
@@ -1,12 +1,22 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
 // forwardRef ---> wrap the component in which ref want to pass. 
                 // Inner function take 2 arguments.
                 // 1) Props & 2) ref
 
 const ResultModal =  forwardRef(function ResultModal({result, targetTime}, ref) {
+    const dialog = useRef();
+    
+    useImperativeHandle(ref, () => {
+        return {
+            open: () => {
+                dialog.current.showModal();
+            }
+        }
+    })
+
     return (
-        <dialog ref={ref} className="result-modal">
+        <dialog ref={dialog} className="result-modal">
             <h2>You {result}</h2>
             <p>The target time was <strong>{targetTime} seconds.</strong></p>
             <p>You stopped the timer with <strong>X second left.</strong></p>
--- 10-Refs-and-Portals/src/components/TimerChallenge.jsx
+++ 10-Refs-and-Portals/src/components/TimerChallenge.jsx
@@ -13,7 +13,7 @@ export default function TimerChallenge({ title, targetTime }) {
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
-      dialog.current.showModal();
+      dialog.current.open();
     }, targetTime * 1000);
 
     setTimerStarted(true);
