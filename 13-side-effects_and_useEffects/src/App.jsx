107d80b34ac27687c1434668e88cf450b20714dd
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-14 18:50:32 +0530

    Fixing a Small Bug
    The <dialog> element can also be closed by pressing the ESC key on the keyboard. In that case, the dialog will disappear but the state passed to the open prop (i.e., the modalIsOpen state) will not be set to false.
    
    Therefore, the modal can't be opened again (because modalIsOpen still is true - the UI basically now is not in sync with the state anymore).
    
    To fix this issue, we must listen to the modal being closed by adding the built-in onClose prop to the <dialog>. The event is then 'forwarded' to the App component by accepting a custom onClose prop on the Modal component.
    
    Modal Component:- <dialog className='modal' ref={dialog} onClose={onClose}>
    App Component:- <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>

--- 13-side-effects_and_useEffects/src/App.jsx
+++ 13-side-effects_and_useEffects/src/App.jsx
@@ -83,7 +83,7 @@ function App() {
 
   return (
     <>
-      <Modal open={modalIsOpen}>
+      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
