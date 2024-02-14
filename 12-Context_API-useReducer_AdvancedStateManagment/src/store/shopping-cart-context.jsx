9b72a526a56632b366e765a846effaf3e331b0e4
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-14 11:58:17 +0530

    Consuming Context.

--- 12-Context_API-useReducer_AdvancedStateManagment/src/store/shopping-cart-context.jsx
+++ 12-Context_API-useReducer_AdvancedStateManagment/src/store/shopping-cart-context.jsx
@@ -1,5 +1,6 @@
 import { createContext } from 'react';
 
 export const CartContext = createContext({
+    // Default value is useful for suggesting. It is not used when a value is provided.
     items: [],
 });
