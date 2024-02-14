d0f2d0b084c8f174ab110e070d06129351a9091a
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-14 19:09:30 +0530

    When dialoug is apper then automatically delete selected place if user not intereact.
    - If user interact then cancel the timer.

--- 13-side-effects_and_useEffects/src/components/DeleteConfirmation.jsx
+++ 13-side-effects_and_useEffects/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,22 @@
+import { useEffect } from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  // useEffect is used to perform side effects in function components.
+  // delete the selected place after 3 seconds.
+  useEffect(() => {
+    console.log("Timer is start");
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, 3000);
+
+    // clean up function.
+    // That will be called when the component is unmounted.
+    return () => {
+      console.log("clean up");
+      clearTimeout(timer);
+    }
+  },[]);
+  
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
--- 13-side-effects_and_useEffects/src/components/Modal.jsx
+++ 13-side-effects_and_useEffects/src/components/Modal.jsx
@@ -14,7 +14,7 @@ function Modal({ open, children, onClose }) {
 
   return createPortal(
     <dialog className="modal" ref={dialog} onClose={onClose}>
-      {children}
+      {open ? children : null}
     </dialog>,
     document.getElementById('modal')
   );
