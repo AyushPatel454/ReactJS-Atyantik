8de3a0b9dd185270fa37f20da9839f84c1ff7073
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-15 12:09:12 +0530

    Shuffling Answers(options of the question) & Adding Quiz Logic.
    - Quiz Logic:- when all question are completed then display summry of quiz. Not go to next question.

--- 14-Quiz-App/src/components/Quiz.jsx
+++ 14-Quiz-App/src/components/Quiz.jsx
@@ -1,10 +1,29 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
+import QuizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
+  // for selecting the current question.
   const activeQuestionIndex = userAnswers.length;
+
+  // checking quiz is complete or not.
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  // if quiz is complete, display the summary component.
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={QuizCompleteImg} alt="Quiz Completed Img" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  // shuffling the options of current question.
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5); // Math.random() returns a number between 0 and 1 (excluding 1) - 0.5 will return a number between -0.5 and 0.5 - this will randomly return a positive or negative number - so we it sort can be in ascending or descending order.
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
@@ -17,7 +36,7 @@ export default function Quiz() {
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
