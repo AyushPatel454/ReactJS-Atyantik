35f9c0a1921a383d4acd16f0a050ddb98d47c73a
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-16 14:28:59 +0530

    Send HTTP Request. (But it's wrong)
    - sending http request. (working)
    - Why wrong ? because when ever response is come we update the state (setAvailablePlaces) and it re-render component & we again fetch the data & again update state and so on...
    - Solution:- useEffect() let's see in next commit.

--- 15-sending-HTTP-request/src/components/AvailablePlaces.jsx
+++ 15-sending-HTTP-request/src/components/AvailablePlaces.jsx
@@ -1,10 +1,22 @@
-import Places from './Places.jsx';
+import { useState } from "react";
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  // ---> fetching data from the server
+  fetch("http://localhost:3000/places")
+    .then((response) => {
+      return response.json(); // <--- returns a promise
+    })
+    .then((resData) => {
+      setAvailablePlaces(resData.places); // response data contain object with places key. (check app.js file in backend folder for more details.)
+    });
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
