32904f2d2b9f32cdaf765c7f1a5b489e514dd0b4
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-16 16:49:56 +0530

    Delete a place from server.

--- 15-sending-HTTP-request/src/App.jsx
+++ 15-sending-HTTP-request/src/App.jsx
@@ -57,8 +57,16 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
     );
 
+    try {
+      const responseFromServer = await updateUsersPlaces(userPlaces.filter((place) => place.id !== selectedPlace.current.id));
+    } catch (error) {
+      setUserPlaces(userPlaces);
+      setErrorUpdatingPlaces({message: error.message || "Failed to delete place."});
+    }
+    
+
     setModalIsOpen(false);
-  }, []);
+  }, [userPlaces]);
 
   function handleError() {
     setErrorUpdatingPlaces(null);
