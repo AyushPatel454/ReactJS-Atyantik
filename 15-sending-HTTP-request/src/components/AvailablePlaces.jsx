f26b0666f668ce4f52c978125702a914f1a8dcf3
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-16 15:48:46 +0530

    Taking location permission from user. & sort the places and display places by nearest ones.

--- 15-sending-HTTP-request/src/components/AvailablePlaces.jsx
+++ 15-sending-HTTP-request/src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false); // loading state.
@@ -15,31 +16,46 @@ export default function AvailablePlaces({ onSelectPlace }) {
     async function fetchData() {
       setIsFetching(true); // for display loading text.
       try {
-        const response = await fetch("http://localhost:3000/placesss"); // <--- returns a promise
+        const response = await fetch("http://localhost:3000/places"); // <--- returns a promise
         const resData = await response.json(); // <--- returns a promise
 
         if (!response.ok) {
           // if response status is not ok (200-299) then throw an error.
-          throw new Error('Failed to fetch places.');
+          throw new Error("Failed to fetch places.");
         }
-
-        setAvailablePlaces(resData.places); // response data contain object with places key. (check app.js file in backend folder for more details.)
+        
+        // ---> get current user location.
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces); // response data contain object with places key. (check app.js file in backend folder for more details.)
+          setIsFetching(false); // for hide loading text & display places (data).
+        });
       } catch (err) {
-        setError({ message: err.message || "Could not fetch places, Please try again later." });
+        setError({
+          message:
+            err.message || "Could not fetch places, Please try again later.",
+        });
+        setIsFetching(false);
       }
+
       
-      setIsFetching(false); // for hide loading text & display places (data).
     }
 
     fetchData();
   }, []);
 
   if (error) {
-    return (<Error
-      title="Error Occurred!"
-      message={error.message}
-      onConfirm={() => {}}
-    />);
+    return (
+      <Error
+        title="Error Occurred!"
+        message={error.message}
+        onConfirm={() => {}}
+      />
+    );
   }
 
   return (
