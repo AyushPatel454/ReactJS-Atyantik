16d897d8fced7a73b8aa2e502cc52a68251853a5
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-16 16:19:50 +0530

    Sending Data with POST/PUT request.
    - when we select places that time we update the state & also store the all selected places in the backend/user-places.json file.
    - the the api in app.js
    
    app.put('/user-places', async (req, res) => {
      const places = req.body.places;
    
      await fs.writeFile('./data/user-places.json', JSON.stringify(places));
    
      res.status(200).json({ message: 'User places updated!' });
    });

--- 15-sending-HTTP-request/src/App.jsx
+++ 15-sending-HTTP-request/src/App.jsx
@@ -5,6 +5,7 @@ import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
+import { updateUsersPlaces } from './http.js';
 
 function App() {
   const selectedPlace = useRef();
@@ -22,7 +23,7 @@ function App() {
     setModalIsOpen(false);
   }
 
-  function handleSelectPlace(selectedPlace) {
+  async function handleSelectPlace(selectedPlace) {
     setUserPlaces((prevPickedPlaces) => {
       if (!prevPickedPlaces) {
         prevPickedPlaces = [];
@@ -32,6 +33,15 @@ function App() {
       }
       return [selectedPlace, ...prevPickedPlaces];
     });
+
+    try {
+      const responseMessage = await updateUsersPlaces([selectedPlace, ...userPlaces]);
+      console.log(responseMessage);
+    } catch (error) {
+      // handle error.
+      console.log("Error occurred while updating user places.",error.message);
+    }
+    
   }
 
   const handleRemovePlace = useCallback(async function handleRemovePlace() {
--- 15-sending-HTTP-request/src/http.js
+++ 15-sending-HTTP-request/src/http.js
@@ -9,3 +9,21 @@ export async function fetchAvailablePlaces() {
 
     return resData.places;
 }
+
+export async function updateUsersPlaces(places) {
+    const response = await fetch('http://localhost:3000/user-places',{
+        method: 'PUT',
+        body: JSON.stringify({places: places}),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const resData = await response.json();
+
+    if(!response.ok) {
+        throw new Error("Failed to update user data.");
+    }
+
+    return resData.message;
+}
