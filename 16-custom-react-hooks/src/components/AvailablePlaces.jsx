dd2957cb01b0f1d2d7deb6bd982b2d902c7ac88a
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-19 11:09:14 +0530

    Creating Flexible custom hook.
    In this commit we creating normal function inside the AvailablePlaces.jsx.
    Which is fetchAvailablePlaces & sort then and return places as a promise.
    So, when we ues custom hook that time we pass this function (fetchSortedPlace). so custom hook directly call this function retrive data.

--- 16-custom-react-hooks/src/components/AvailablePlaces.jsx
+++ 16-custom-react-hooks/src/components/AvailablePlaces.jsx
@@ -6,23 +6,30 @@ import { sortPlacesByDistance } from "../loc.js";
 import { fetchAvailablePlaces } from "../http.js";
 import { useFetch } from "../hooks/useFetch.js";
 
-// navigator.geolocation.getCurrentPosition((position) => {
-//   const sortedPlaces = sortPlacesByDistance(
-//     places,
-//     position.coords.latitude,
-//     position.coords.longitude
-//   );
-//   setAvailablePlaces(sortedPlaces);
-//   setIsFetching(false);
-// });
+// this function fetches available places and sorts them by distance to the user's location.
+// & it's return a promise.
+async function fetchSortedPlaces() {
+  const places = await fetchAvailablePlaces();
+
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+
+      resolve(sortedPlaces);
+    });
+  });
+}
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const {
     isFetching,
     fetchData: availablePlaces,
-    setFetchData: setAvailablePlaces,
     error,
-  } = useFetch(fetchAvailablePlaces, []);
+  } = useFetch(fetchSortedPlaces, []);
 
   if (error) {
     return <Error title="An error occurred!" message={error.message} />;
