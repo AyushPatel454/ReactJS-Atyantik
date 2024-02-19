548cf4c8815ae197b3ff558aefbfa0e8048a33a6
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-19 12:17:09 +0530

    Managing & Getting User Input via State & Generic Handlers (state with object value.)

--- 17-working-with-Form-and-User-Input/src/components/Login.jsx
+++ 17-working-with-Form-and-User-Input/src/components/Login.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
+
 export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [enteredPassword, setEnteredPassword] = useState("");
+
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
 
   // handle form submit.
   function handleSubmit(event) {
     event.preventDefault(); // prevent default form submission. (not reload the page - not send form data.)
     console.log("Submit!");
+
+    console.log("User Email: " + enteredValues.email + " Password: "+enteredValues.password);
+  }
+
+  // when input field is change ---> update the state with current input field values.
+  function handleInputChange(identifier, event) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: event.target.value,
+    }));
   }
 
   return (
@@ -13,12 +32,24 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={enteredValues.email}
+            onChange={(event) => handleInputChange("email", event)}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={enteredValues.password}
+            onChange={(event) => handleInputChange("password", event)}
+          />
         </div>
       </div>
 
