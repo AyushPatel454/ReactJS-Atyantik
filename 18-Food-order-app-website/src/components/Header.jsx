72138146b553a11f85fb4fc2486bf6dcef5876c8
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-20 15:36:29 +0530

    Opening the cart in the Modal via a New Context.

--- 18-Food-order-app-website/src/components/Header.jsx
+++ 18-Food-order-app-website/src/components/Header.jsx
@@ -2,13 +2,19 @@ import { useContext } from 'react';
 import logoImg from '../assets/logo.jpg';
 import Button from './UI/Button.jsx';
 import CartContext from '../store/CartContext.jsx';
+import UserProgressContext from '../store/UserProgressContext.jsx';
 
 export default function Header() {
     const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
 
     const totalItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
         return totalNumberOfItems + item.quantity; 
     }, 0);
+
+    function handleShowCart() {
+        userProgressCtx.showCart();
+    }
 
     return (
     <header id="main-header">
@@ -18,7 +24,7 @@ export default function Header() {
         </div>
 
         <nav>
-            <Button textOnly={true}>Cart ({totalItems})</Button>
+            <Button textOnly={true} onClick={handleShowCart}>Cart ({totalItems})</Button>
         </nav>
         
       
--- 18-Food-order-app-website/src/components/UI/Modal.jsx
+++ 18-Food-order-app-website/src/components/UI/Modal.jsx
@@ -4,9 +4,14 @@ import { createPortal } from "react-dom";
 export default function Modal({ children, open, className = '' }) {
     const dialog = useRef();
     useEffect(() => {
+        const modal = dialog.current;
         if(open) {
-            dialog.current.showModal();
+            modal.showModal();
         }
+
+        // clean up function.
+        // close the modal when open = '' (false) or when component is unmounted. (unmounted means the component is removed from the DOM)
+        return () => modal.close();
     }, [open]);
 
   return createPortal(
