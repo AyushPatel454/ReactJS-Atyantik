5dd0d9917913c98306ea7b852e45713ed9f582d9
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-21 11:05:06 +0530

    New Learning: Redux.
    This JavaScript code is using Redux, a library for managing state in applications. Here's a simple explanation:
    
    1. A Redux store is created with redux.createStore(counterReducer). This store holds the entire state of the application. The counterReducer is a function that determines how the state should change based on certain actions.
    
    2. A function counterSubscriber is defined. This function gets the latest state from the store and logs it to the console. This function will be used to listen for any changes in the store's state.
    
    3. store.subscribe(counterSubscriber) is used to subscribe the counterSubscriber function to the store. This means that counterSubscriber will be called every time an action is dispatched to the store and the state changes.
    
    4. Finally, store.dispatch({type: 'increment'}) is used to dispatch an action to the store. This action is an object with a type property of 'increment'. When this action is dispatched, the counterReducer function will be called with the current state and this action, and it will return the new state. In this case, it's expected that the counter in the state will be incremented.

---
+++ 19-redux-alternativeOfContextAPI/redux-demo.js
@@ -0,0 +1,30 @@
+const redux = require('redux'); // node js syntax for importing a 3rd party package. (module)
+
+/* Reducers are functions that take in the current state and an action, and return a new state.
+   Reducers are pure functions. They only return the new state, and do not modify the old state.
+   Reducers are called by the store.dispatch() method.
+   Reducers are used to manage the state of the application.
+   Reducers are used to update the state based on the action.
+   Reducers are used to handle the asynchronous actions. */
+
+//    take initial value of state ---> state = {counter: 0}
+const counterReducer = (state = {counter: 0}, action) => {
+    return {
+        counter: state.counter + 1,
+    }
+}
+
+// create a store and pass the reducer to the store.
+const store = redux.createStore(counterReducer);
+
+// for subscribing to the store. so that we can listen to the changes in the store.
+const counterSubscriber = () => {
+    const latestState = store.getState();
+    console.log(latestState);
+}
+
+// subscribe to the store.
+store.subscribe(counterSubscriber);
+
+// dispatch an action to the store. (means changing the state of the store)
+store.dispatch({type: 'increment'}); // {counter: 2}
