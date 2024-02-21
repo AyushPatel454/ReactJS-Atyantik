9c424a7f15c8df0d53f4084b6445ef4ea22fd5eb
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-21 12:02:05 +0530

    Using Redux Data in React Components.
    useSelector is a hook provided by the react-redux library. It allows you to extract data from the Redux store state, using a selector function.
    
    Why we use it: We use useSelector to access the state in the Redux store from our components. This allows us to decouple our components from the state management and makes them more reusable and easier to test.
    
    - How to use it:The useSelector hook takes a single argument: a selector function. This function has the entire Redux store state as its argument, and should return the piece of state this component needs.
    
    const counter = useSelector(state => state.counter);
    
    In this example, the selector function is (state) => state.counter, which means it will extract the counter value from the state.
    
    When to use it: You should use useSelector whenever you need to access data from the Redux store in a functional component. It's a part of the React-Redux hooks API, which is a more modern approach to accessing store data than the connect higher-order component.

--- 20-redux/src/components/Counter.js
+++ 20-redux/src/components/Counter.js
@@ -1,12 +1,19 @@
+import { useSelector } from 'react-redux';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  // set automatically subscription to the store.
+  // it will be called whenever the store changes
+  // so that it can get the latest state and re-render the component
+  const counter = useSelector(state => state.counter);
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
