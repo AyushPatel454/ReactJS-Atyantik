bdd130518a419b5f61737dd56b66eed593443cde
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-21 12:12:31 +0530

    Dispatching Actions from inside the Components.
    - when increment or decrement button is click then update the value by 1.

--- 20-redux/src/components/Counter.js
+++ 20-redux/src/components/Counter.js
@@ -1,4 +1,4 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import classes from './Counter.module.css';
 
@@ -8,12 +8,29 @@ const Counter = () => {
   // so that it can get the latest state and re-render the component
   const counter = useSelector(state => state.counter);
 
+  const  dispatch = useDispatch(); // return a function that we can call to dispatch an action.
+
+  // it will be called whenever the increment button is clicked
+  const incrementHandler = () => {
+    // dispatch an action to the store. the action is pass to the reducer function in the store & the reducer function will update the state.
+    dispatch({type: 'increment'});
+  };
+
+  // when the decrement button is clicked, it will dispatch an action to the store.
+  const decrementHandler = () => {
+    dispatch({type: 'decrement'});
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
