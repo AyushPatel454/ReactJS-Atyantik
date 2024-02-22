156640adbf02eadc201c0bfb6b6ee40719f8d47d
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-22 16:34:42 +0530

    Using an Action Creator Thunk.
    a thunk is a middleware that allows you to write action creators that return a function instead of an action. The thunk can be used to delay the dispatch of an action, or to dispatch only if a certain condition is met. This functionality makes it possible to handle complex logic in your action creators.

--- 21-advanced-redux/src/App.js
+++ 21-advanced-redux/src/App.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { notificationActions } from './store/notification-slice.js';
+import { sendCartData } from './store/cart-slice.js';
 import Notification from './components/UI/Notification.js';
 
 let initial = true;
@@ -16,58 +16,12 @@ function App() {
 
   // whenever our cart changes, we want to send an HTTP request to store the cart data in the (Firebase - Realtime database) database.
   useEffect(() => {
-
     if (initial) {
       initial = false;
       return;
     }
 
-    const sendCartData = async () => {
-      dispatch(
-        notificationActions.showNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data!',
-        })
-      );
-      const response = await fetch('https://redux-practice-d7720-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
-        method: 'PUT',
-        body: JSON.stringify(cart),
-      });
-
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.');
-      }
-
-      dispatch(
-        notificationActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cart data successfully!',
-        })
-      );
-
-      // hide the notification after 3 seconds
-      setTimeout(() => {
-        dispatch(notificationActions.hideNotification());
-      }, 3000);
-    };
-
-    sendCartData().catch((error) => {
-      console.log('error', error);
-      dispatch(
-        notificationActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed!',
-        })
-      );
-
-      // hide the notification after 3 seconds
-      setTimeout(() => {
-        dispatch(notificationActions.hideNotification());
-      }, 3000);
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
--- 21-advanced-redux/src/store/cart-slice.js
+++ 21-advanced-redux/src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { notificationActions } from './notification-slice.js';
 
 const initialCartState = {
     quantity: 0,
@@ -50,11 +51,72 @@ const cartSlice = createSlice({
             state.price -= state.items[index].price;
 
             // remove the item if the quantity of item is 0.
-            if(state.items[index].quantity === 0) {
+            if (state.items[index].quantity === 0) {
                 state.items = state.items.filter(item => item.id !== action.payload.id);
             }
         },
     }
-})
+});
+
+
+export const sendCartData = (cart) => {
+    return async (dispatch) => {
+        dispatch(
+            notificationActions.showNotification({
+                status: 'pending',
+                title: 'Sending...',
+                message: 'Sending cart data!',
+            })
+        );
+
+        // send the cart data to the server
+        const sendRequest = async () => {
+            const response = await fetch('https://redux-practice-d7720-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
+                method: 'PUT',
+                body: JSON.stringify(cart),
+            });
+
+            if (!response.ok) {
+                throw new Error('Sending cart data failed.');
+            }
+        }
+
+        // call the sendRequest function.
+        try {
+            await sendRequest();
+
+            dispatch(
+                notificationActions.showNotification({
+                    status: 'success',
+                    title: 'Success!',
+                    message: 'Sent cart data successfully!',
+                })
+            );
+    
+            // hide the notification after 3 seconds
+            setTimeout(() => {
+                dispatch(notificationActions.hideNotification());
+            }, 3000);
+
+        } catch (error) {
+            dispatch(
+                notificationActions.showNotification({
+                    status: 'error',
+                    title: 'Error!',
+                    message: 'Sending cart data failed!',
+                })
+            );
+
+            // hide the notification after 3 seconds
+            setTimeout(() => {
+                dispatch(notificationActions.hideNotification());
+            }, 3000);
+        }
+
+        
+    }
+}
+
+
 export default cartSlice.reducer;
 export const cartActions = cartSlice.actions;
