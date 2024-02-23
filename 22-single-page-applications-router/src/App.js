1c454be02186aa9f58978a1b19b9c40cc5b7b4cd
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-23 10:53:01 +0530

    Back to our previous way to defining the routes.

--- 22-single-page-applications-router/src/App.js
+++ 22-single-page-applications-router/src/App.js
@@ -1,20 +1,11 @@
-import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Product.js";
 
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<ProductsPage />} />
-  </Route>
-);
-
-// const router = createBrowserRouter([
-//   {path: "/", element: <HomePage />},
-//   {path:"/products", element: <ProductsPage />},
-// ])
-
-const router = createBrowserRouter(routeDefinitions);
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
