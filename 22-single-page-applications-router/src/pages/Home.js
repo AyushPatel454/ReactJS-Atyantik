91aba86f3b33a19d5b86c56ff4c33f850b40e7f1
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-23 12:12:33 +0530

    Navigate Programatically.
    - using ---> useNavigate()

--- 22-single-page-applications-router/src/pages/Home.js
+++ 22-single-page-applications-router/src/pages/Home.js
@@ -1,8 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Home() {
+    const navigate = useNavigate();
+
+    function handleNavigate() {
+        navigate('/products');
+    }
+
     return <>
-    <h1>My Home page.</h1>
-    <p>Go to products page. <Link to="/products">Products...</Link></p>
+        <h1>My Home page.</h1>
+        <p>Go to products page. <Link to="/products">Products...</Link></p>
+        <button onClick={handleNavigate}> Navigate to Products</button>
     </>;
 }
