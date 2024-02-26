a6918d3c7a32fd301a258d4b2219e8b60b371021
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-26 17:36:22 +0530

    Updating the UI based on the Auth status.

--- 24-authentication-to-react-app/frontend/src/App.js
+++ 24-authentication-to-react-app/frontend/src/App.js
@@ -15,12 +15,15 @@ import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 import AuthenticationPage, {action as authAction} from './pages/Authentication';
 import { action as logoutAction } from './pages/Logout';
+import { tokenLoader } from './utils/auth';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    id: 'root',
+    loader: tokenLoader,
     children: [
       { index: true, element: <HomePage /> },
       {
--- 24-authentication-to-react-app/frontend/src/utils/auth.js
+++ 24-authentication-to-react-app/frontend/src/utils/auth.js
@@ -3,3 +3,7 @@ export function getAuthToken() {
     const token = localStorage.getItem('token');
     return token;
 }
+
+export function tokenLoader() {
+    return getAuthToken();
+}
