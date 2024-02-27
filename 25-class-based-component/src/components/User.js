12231ddb14e6cd6d5cab72c1f013a6cb365dcf08
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-27 13:09:10 +0530

    Use Life cycle methods.

--- 25-class-based-component/src/components/User.js
+++ 25-class-based-component/src/components/User.js
@@ -2,6 +2,13 @@ import { Component } from 'react';
 import classes from './User.module.css';
 
 class User extends Component {
+
+  // (Life cycle method)
+  // if will be execute once when the component is unmounted (removed from the DOM).
+  componentWillUnmount() {
+    console.log("User will unmount...!!"); // it will execute when the component is removed from the DOM.
+  }
+
   render() {
     return <li className={classes.user}>{this.props.name}</li>;
   }
--- 25-class-based-component/src/components/UserFinder.js
+++ 25-class-based-component/src/components/UserFinder.js
@@ -1,36 +1,65 @@
-import { Fragment, useState, useEffect } from 'react';
+import { Fragment, Component } from 'react';
 
 import Users from './Users';
 import classes from './UserFinder.module.css';
 
 const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
+    { id: 'u1', name: 'Max' },
+    { id: 'u2', name: 'Manuel' },
+    { id: 'u3', name: 'Julie' },
 ];
 
-const UserFinder = () => {
-  const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
-  const [searchTerm, setSearchTerm] = useState('');
+class UserFinder extends Component {
+    // --> define initial state.
+    constructor() {
+        super();
+        this.state = {
+            filteredUsers: [],
+            searchTerm: ''
+        };
+    }
 
-  useEffect(() => {
-    setFilteredUsers(
-      DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
-    );
-  }, [searchTerm]);
+    // (Life cycle method)
+    // if will be execute once when the component is mounted.
+    // it like a ---> useEffect( () => {}, [])
+    componentDidMount() {
+        // send HTTP request... ... ...
+        this.setState({ filteredUsers: DUMMY_USERS });
+    }
 
-  const searchChangeHandler = (event) => {
-    setSearchTerm(event.target.value);
-  };
+    // (Life cycle method)
+    // if will be execute when ever the component is updated.
+    // it like a ---> useEffect( () => {}, [dependency])
+    // in below my dependency is this.state.searchTerm (if it's value is changed then it will execute)
+    componentDidUpdate(prevProps, prevState) {
+        // --> check if the previous state is not equal to the current state.
+        // if equal then no need to update the state.
+        // if now equal then update the state.
+        if (prevState.searchTerm !== this.state.searchTerm) {
+            this.setState({
+                filteredUsers: DUMMY_USERS.filter((user) => user.name.includes(this.state.searchTerm))
+            });
+        }
+    }
 
-  return (
-    <Fragment>
-      <div className={classes.finder}>
-        <input type='search' onChange={searchChangeHandler} />
-      </div>
-      <Users users={filteredUsers} />
-    </Fragment>
-  );
-};
+    // --> define method.
+    searchChangeHandler(event) {
+        this.setState({
+            searchTerm: event.target.value
+        });
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <div className={classes.finder}>
+                    <input type='search' onChange={this.searchChangeHandler.bind(this)} />
+                </div>
+                <Users users={this.state.filteredUsers} />
+            </Fragment>
+        );
+    }
+
+}
 
 export default UserFinder;
