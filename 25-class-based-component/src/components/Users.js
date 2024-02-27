e11e9cca4d9495e257f04465912e73778d805cb1
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-27 12:16:23 +0530

    Working with state & events.
    - how to define state variables. (always be an object in which we define all the properties which we want to use or update when something happen.)
    - how to use setState for updating the state properties. (It's not overwrite or replace property but it will merge with old  state properties.)
    - how to access state properties.
    - how to call methods (function) - use .bind(this) - because we need to specify this value is current context not an element value.

--- 25-class-based-component/src/components/Users.js
+++ 25-class-based-component/src/components/Users.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Component, useState } from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
@@ -9,29 +9,43 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Julie' },
 ];
 
-const Users = () => {
-  const [showUsers, setShowUsers] = useState(true);
+class Users extends Component {
+  constructor() {
+    // super is used to call the constructor of the parent class. -- needed other wise it will throw an error.
+    super();
+    // you can done initialization here.
+    // state is always an Object. // & it's name always  be state. (so when we want to define state then we have to use this.state = {})
+    this.state = {
+      showUsers: true,
+    };
+  }
 
-  const toggleUsersHandler = () => {
-    setShowUsers((curState) => !curState);
-  };
+  // ---> Defining method.
+  toggleUsersHandler() {
+    // ---> Update the state.
+    // this.state.showUsers = !this.state.showUsers; // Don't do this // NOT use
+    this.setState((curState) => {
+      return {
+        showUsers: !curState.showUsers, // it will merge with existing state not replace it.
+      }
+    })
+  }
+  render() {
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
 
-  const usersList = (
-    <ul>
-      {DUMMY_USERS.map((user) => (
-        <User key={user.id} name={user.name} />
-      ))}
-    </ul>
-  );
-
-  return (
-    <div className={classes.users}>
-      <button onClick={toggleUsersHandler}>
-        {showUsers ? 'Hide' : 'Show'} Users
+    return (<div className={classes.users}>
+      <button onClick={this.toggleUsersHandler.bind(this)}> {/* this.toggleUsersHandler.bind(this) is used to bind the context of this to the function */}
+        {this.state.showUsers ? 'Hide' : 'Show'} Users
       </button>
-      {showUsers && usersList}
-    </div>
-  );
-};
+      {this.state.showUsers && usersList}
+    </div>)
+  }
+}
 
 export default Users;
