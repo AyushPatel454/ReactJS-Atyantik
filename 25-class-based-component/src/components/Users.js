4dc20ab9c4d7d23a3da918f60d7419bc840ba2ef
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-27 12:37:17 +0530

    Clas-based component Life cycle.
    - add new file. we will convert that functional component into the clas based component.
    
    ---> Life cycle of clas based component.
    
    1> componentDidMount(): This method is called once the component has been rendered to the DOM. It's a good place to make network requests or set up subscriptions.
       It's like ---> useEffect( () => {} , [])
    
    2> componentDidUpdate(prevProps, prevState, snapshot): This method is called after the component updates and re-renders. It's a good place to do network requests as long as you compare current props to previous props.
       It's like ---> useEffect ( () => {} , [someValueDependecies] )
    
    3> componentWillUnmount(): This method is called before a component is destroyed. It's a good place to clean up any subscriptions or events to prevent memory leaks.
       It's like ---> clean up function of useEffect. ---> useEffect( () => { return () => {}; } , [])

--- 25-class-based-component/src/components/Users.js
+++ 25-class-based-component/src/components/Users.js
@@ -1,13 +1,7 @@
-import { Component, useState } from 'react';
+import { Component } from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
-
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
 
 class Users extends Component {
   constructor() {
@@ -33,7 +27,7 @@ class Users extends Component {
   render() {
     const usersList = (
       <ul>
-        {DUMMY_USERS.map((user) => (
+        {this.props.users.map((user) => (
           <User key={user.id} name={user.name} />
         ))}
       </ul>
