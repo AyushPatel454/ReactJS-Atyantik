8fcf1541e9e375dd5b799acaf70585ce5fb9daf9
Ayush Patel <aayushpatel25190@gmail.com>
2024-02-28 13:09:11 +0530

    Optimise code for todo-app-without global storage.

--- Todo-app-classBasedComponent/todo-app-without-global-storage/src/App.js
+++ Todo-app-classBasedComponent/todo-app-without-global-storage/src/App.js
@@ -4,119 +4,71 @@ import Header from './components/Header';
 import Form from './components/Form';
 import TodoList from './components/TodosList.js';
 
-// ---> for updation in localstorage.
-function updateLocalStorage(obj) {
-  localStorage.setItem("todoList", JSON.stringify(obj));
-}
-
 class App extends Component {
   constructor() {
     super();
-    this.state = {
+    const storedState = JSON.parse(localStorage.getItem("todoList")) || {
       todos: [],
       totalTodo: 0,
       doneTodo: 0,
       undoneTodo: 0,
-    }
+    };
+
+    this.state = storedState;
 
     this.updateTodoHandler = this.updateTodoHandler.bind(this);
     this.addTodoHandler = this.addTodoHandler.bind(this);
     this.deleteTodoHandler = this.deleteTodoHandler.bind(this);
   }
 
-  // ---> Load todos from the loacal storage when this Component is loaded.
-  componentDidMount() {
-    const todoList = JSON.parse(localStorage.getItem("todoList")) || {};
-    this.setState({
-        todos: todoList.todos || [],
-        totalTodo: todoList.totalTodo || 0,
-        doneTodo: todoList.doneTodo || 0,
-        undoneTodo: todoList.undoneTodo || 0,
-    });
-}
-
   // ---> add todo
   addTodoHandler(todo) {
-    const todoList = [...this.state.todos]; // make deep copy of todo list.
-    todoList.push(todo); // add new todo.
+    this.setState(prevState => {
+      const todos = [...prevState.todos, todo]; // deep copy of prevState + new Todo
+      const totalTodo = prevState.totalTodo + 1; // total todo
+      const undoneTodo = prevState.undoneTodo + 1; // remaining todo
+      const doneTodo = prevState.doneTodo; // done todo
 
-    console.log(todoList);
+      // new state (updated state)
+      const newState = { todos, totalTodo, doneTodo, undoneTodo };
 
-    // update the state.
-    this.setState({
-      todos: todoList,
-      totalTodo: this.state.totalTodo + 1,
-      undoneTodo: this.state.undoneTodo + 1,
+      // update in local storage
+      localStorage.setItem("todoList", JSON.stringify(newState));
+
+      // return updated state so it can be used by other component also.
+      return newState;
     });
-
-    // update the local storage.
-    const obj = {
-      todos: todoList,
-      totalTodo: this.state.totalTodo + 1,
-      doneTodo: this.state.doneTodo,
-      undoneTodo: this.state.undoneTodo + 1,
-    };
-
-    updateLocalStorage(obj);
   }
 
   // ---> update todo
   updateTodoHandler(id, title = '') {
-    const todoIndex = this.state.todos.findIndex((todo) => todo.id === id); // find the index of todo.
-    const todo = { ...this.state.todos[todoIndex] }; // copy the todo.
-
-    if (title !== '') {
-      todo.title = title;
-    } else {
-      todo.done = !todo.done; // make to do done or undone.
-    }
-
-    const isDone = todo.done; // check if the todo is done or undone.
-    const todoList = [...this.state.todos]; // make deep copy of todo list.
-    todoList[todoIndex] = todo; // update the todo list.
-
-    // update the state.
-    this.setState({
-      todos: todoList,
-      doneTodo: title !== '' ? this.state.doneTodo : isDone ? this.state.doneTodo + 1 : this.state.doneTodo - 1,
-      undoneTodo: title !== '' ? this.state.undoneTodo : isDone ? this.state.undoneTodo - 1 : this.state.undoneTodo + 1,
+    this.setState(prevState => {
+      const todos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          const done = title !== '' ? todo.done : !todo.done; // check i change title or status of todo.
+          return { ...todo, title: title !== '' ? title : todo.title, done };
+        }
+        return todo;
+      });
+      const doneTodo = todos.filter(todo => todo.done).length;
+      const undoneTodo = prevState.totalTodo - doneTodo;
+      const newState = { todos, totalTodo: prevState.totalTodo, doneTodo, undoneTodo };
+      localStorage.setItem("todoList", JSON.stringify(newState));
+      return newState;
     });
-
-    // update the local storage.
-    const obj = {
-      todos: todoList,
-      totalTodo: this.state.totalTodo,
-      doneTodo: title !== '' ? this.state.doneTodo : isDone ? this.state.doneTodo + 1 : this.state.doneTodo - 1,
-      undoneTodo: title !== '' ? this.state.undoneTodo : isDone ? this.state.undoneTodo - 1 : this.state.undoneTodo + 1,
-    };
-
-    updateLocalStorage(obj);
   }
 
   // delete todo
   deleteTodoHandler(id) {
-    const todoIndex = this.state.todos.findIndex((todo) => todo.id === id); // find the index of todo.
-    const isDone = this.state.todos[todoIndex].done; // check if the todo is done or undone.
-
-    const todoList = [...this.state.todos.filter((todo) => todo.id !== id)];
-
-    // update the state.
-    this.setState({
-      todos: todoList,
-      totalTodo: this.state.totalTodo - 1,
-      doneTodo: isDone ? this.state.doneTodo - 1 : this.state.doneTodo,
-      undoneTodo: isDone ? this.state.undoneTodo : this.state.undoneTodo - 1,
+    this.setState(prevState => {
+      const todos = prevState.todos.filter(todo => todo.id !== id);
+      const totalTodo = prevState.totalTodo - 1;
+      const doneTodo = todos.filter(todo => todo.done).length;
+      const undoneTodo = totalTodo - doneTodo;
+      const newState = { todos, totalTodo, doneTodo, undoneTodo };
+      localStorage.setItem("todoList", JSON.stringify(newState));
+      return newState;
     });
-
-    // update the local storage.
-    const obj = {
-      todos: todoList,
-      totalTodo: this.state.totalTodo - 1,
-      doneTodo: isDone ? this.state.doneTodo - 1 : this.state.doneTodo,
-      undoneTodo: isDone ? this.state.undoneTodo : this.state.undoneTodo - 1,
-    };
-
-    updateLocalStorage(obj);
   }
 
   render() {
