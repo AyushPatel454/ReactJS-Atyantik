b05070ea551661e23e93d7cea80e135ee99ee916
Aayushp454 <aayushpatel25190@gmail.com>
2024-04-30 14:42:47 +0530

    use client: use for solving an error.

--- 29-Foodies-Project/.next/server/webpack-runtime.js
+++ 29-Foodies-Project/.next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("950a55138eb018b0")
+/******/ 		__webpack_require__.h = () => ("033c8c6eb4543a25")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
