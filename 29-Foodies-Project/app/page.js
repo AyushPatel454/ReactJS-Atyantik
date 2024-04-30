20c6fb2857cc929e217a03b6ab1981aee3fa1ab8
Aayushp454 <aayushpatel25190@gmail.com>
2024-04-30 14:41:36 +0530

    Preparing Image Slide show.

--- 29-Foodies-Project/app/page.js
+++ 29-Foodies-Project/app/page.js
@@ -1,12 +1,15 @@
 import Link from 'next/link';
 
 import classes from './page.module.css';
+import ImageSlideshow from '@/components/images/imgae-slideshow';
 
 export default function Home() {
   return (
     <>
       <header className={classes.header}>
-        <div className={classes.slideshow}></div>
+        <div className={classes.slideshow}>
+          <ImageSlideshow />
+        </div>
         <div>
           <div className={classes.hero}>
             <h1>NextLevel Food for NextLevel Foodies</h1>
